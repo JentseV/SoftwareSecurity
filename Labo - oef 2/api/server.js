fb24982e1b411dc189077fd858b1548e0d81a797
JentseV <s113290@ap.be>
2023-02-12 06:42:11 +0100

    Labo opdracht 2

---
+++ Labo - oef 2/api/server.js
@@ -0,0 +1,24 @@
+// simple api server that listens on localhost:5000 and replies with "Hello from api!"
+
+const { json } = require('express');
+const express = require('express');
+const server = express();
+const HOST = "localhost";
+const PORT = "5000";
+
+const reply = "Hello from api!";
+
+
+server.use((request,response,next) => {
+    response.header("Access-Control-Allow-Origin", "http://localhost:5001"); // allow our web app to access our api
+    next();
+})
+
+
+server.get("",(request,response) => {
+    response.status(200).json(reply);
+})
+
+server.listen(PORT,HOST,() => {
+    console.log("Server started listening...");
+})
---
+++ Labo - oef 2/application/app.js
@@ -0,0 +1,18 @@
+// simple web application running on localhost:5001
+
+const express = require("express");
+const app = express();
+const HOST  = "localhost";
+const PORT = "5001";
+
+app.use(express.static(__dirname));
+
+app.get("", (request,response) => {
+    response.status(200).sendFile(path.join(__dirname,'/index/html'));
+})
+
+
+
+app.listen(PORT,HOST,()=> {
+    console.log("Running web app");
+})
